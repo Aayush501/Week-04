12401f5be7f48ec495bece84ae1bf5362fe38d8d
Aayush501 <shrivastavashu501@gmail.com>
2025-02-14 15:46:15 +0530

    [Aayush Kumar Shrivastav] Add . Solution -> Unchecked Exception Problem

--- Day04/Exceptions/src/main/java/org/exception/UncheckedException.java
+++ Day04/Exceptions/src/main/java/org/exception/UncheckedException.java
@@ -1,4 +1,28 @@
 package org.exception;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UncheckedException {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in); // Scanner object
+            // taking input for dividend and divisor
+            System.out.println("Enter The Dividend: ");
+            int dividend = sc.nextInt();
+            System.out.println("Enter The Divisor: ");
+            int divisor = sc.nextInt();
+
+            // performing division
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            System.out.println("Quotient: " + quotient + "\n" + "Remainder: " + remainder);
+        }
+        // handling potential errors
+        catch (ArithmeticException a) {
+            System.out.println("Arithmetic Exception Occurred. This Division Cannot Be Completed.");
+        } catch (InputMismatchException i) {
+            System.out.println("User Entered A Non-Numeric Value.");
+        }
+    }
 }
