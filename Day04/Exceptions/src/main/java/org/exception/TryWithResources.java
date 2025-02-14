5d60bfbb83a8daccd8a3ee5a64ea5311593628e2
Aayush501 <shrivastavashu501@gmail.com>
2025-02-14 15:53:35 +0530

    [Aayush Kumar Shrivastav] Add . Solution -> Try With Resources Problem

--- Day04/Exceptions/src/main/java/org/exception/TryWithResources.java
+++ Day04/Exceptions/src/main/java/org/exception/TryWithResources.java
@@ -1,4 +1,19 @@
 package org.exception;
 
+import java.io.*;
+
 public class TryWithResources {
+    public static void main(String[] args) {
+        String filePath = "info.txt"; // file path of a non-existing file
+        File file = new File(filePath); // File object
+
+        // reading the file and handling the exception
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) { // try with resources
+            // FileReader object wrapped in BufferedReader
+            String line = reader.readLine(); // reading the first line
+            System.out.println(line);
+        } catch (IOException i) { // handling the exception if file not found
+            System.out.println("Error reading file");
+        }
+    }
 }
