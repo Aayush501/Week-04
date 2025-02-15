7d5d17329faf3c3529a1eb4645ff9218bc9a68fd
Aayush501 <shrivastavashu501@gmail.com>
2025-02-15 23:20:31 +0530

    [Aayush Kumar Shrivastav] Add . Validating License Number Plate -> (Solution + Test Code)

--- Day05/Regex/src/main/java/org/regex/LicensePlateNumber.java
+++ Day05/Regex/src/main/java/org/regex/LicensePlateNumber.java
@@ -1,4 +1,31 @@
 package org.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LicensePlateNumber {
+    public static void main(String[] args) {
+        // defining a regex pattern to validate the license plate number
+        String regex = "^[A-Z]{2}[0-9]{4}$";
+
+        // compiling the regex pattern
+        Pattern pattern = Pattern.compile(regex);
+
+        // created a string array to store license plate numbers, that will be validated
+        String [] licensePlateNumber = {"AB1234", "CD3456", "ab1234", "C12345", "ABC123", "12ABCD", "ABCDEF", "Ab1111", "aB1234", "123456"};
+
+        // iterating through the licensePlateNumber array to validate each string
+        for (String license : licensePlateNumber) {
+            if (isValidLicensePlateNumber(license, pattern)) {
+                System.out.println("Valid License Plate Number.");
+            } else {
+                System.out.println("Invalid License Plate Number.");
+            }
+        }
+    }
+
+    public static boolean isValidLicensePlateNumber(String license, Pattern pattern) {
+        Matcher matcher = pattern.matcher(license);
+        return matcher.matches();
+    }
 }
---
+++ Day05/Regex/src/test/java/org/regex/LicensePlateNumberTest.java
@@ -0,0 +1,38 @@
+package org.regex;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LicensePlateNumberTest {
+    // testing the isValidLicensePlateNumber method for valid license plate numbers
+    @ParameterizedTest
+    @ValueSource(strings = {"AB1234", "CD3456", "EF5678"})
+    public void isValidLicensePlateNumber_ValidTest(String test) {
+        // defining a regex pattern to validate the license plate number
+        String regex = "^[A-Z]{2}[0-9]{4}$";
+
+        // compiling the regex pattern
+        Pattern pattern = Pattern.compile(regex);       
+
+        // performing the tests
+        assertTrue(LicensePlateNumber.isValidLicensePlateNumber(test, pattern));
+    }
+
+    // testing the isValidLicensePlateNumber method for invalid license plate numbers
+    @ParameterizedTest
+    @ValueSource(strings = {"A01234", "CD34AB", "5678AB"})
+    public void isValidLicensePlateNumber_InvalidTest(String test) {
+        // defining a regex pattern to validate the license plate number
+        String regex = "^[A-Z]{2}[0-9]{4}$";
+
+        // compiling the regex pattern
+        Pattern pattern = Pattern.compile(regex);
+
+        // performing the tests
+        assertFalse(LicensePlateNumber.isValidLicensePlateNumber(test, pattern));
+    }
+}
