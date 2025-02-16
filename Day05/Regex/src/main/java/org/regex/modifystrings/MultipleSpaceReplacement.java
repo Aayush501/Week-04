5388636155b6cda891dbbb46f397434697da8af3
Aayush501 <shrivastavashu501@gmail.com>
2025-02-16 14:16:16 +0530

    [Aayush Kumar Shrivastav] Add . Multiple Space Replacement -> (Solution + Test)

---
+++ Day05/Regex/src/main/java/org/regex/modifystrings/MultipleSpaceReplacement.java
@@ -0,0 +1,16 @@
+package org.regex.modifystrings;
+
+import java.util.Scanner;
+
+public class MultipleSpaceReplacement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println("String after removing multiple spaces : " + replaceSpaces(text));
+    }
+
+    public static String replaceSpaces(String text){
+        // replacing all extra backspace by one space
+        return text.replaceAll("\\s+" , " ");
+    }
+}
---
+++ Day05/Regex/src/test/java/org/regex/modifystrings/MultipleSpaceReplacementTest.java
@@ -0,0 +1,22 @@
+package org.regex.modifystrings;
+
+import org.junit.jupiter.api.Test;
+import org.regex.LicensePlateNumber;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultipleSpaceReplacementTest {
+    // testing the replaceSpaces method
+    @Test
+    public void isValidLicensePlateNumber_ValidTest() {
+        // performing the tests on multiple testcases
+        String test1 = "My   name    is    Aayush" ;
+        String test2 = "I am       a     Java developer" ;
+        String test3 = "I    am an     engineer." ;
+
+        // performing tests
+        assertEquals("My name is Aayush", MultipleSpaceReplacement.replaceSpaces(test1));
+        assertEquals("I am a Java developer", MultipleSpaceReplacement.replaceSpaces(test2));
+        assertEquals("I am an engineer.", MultipleSpaceReplacement.replaceSpaces(test3));
+    }
+}
