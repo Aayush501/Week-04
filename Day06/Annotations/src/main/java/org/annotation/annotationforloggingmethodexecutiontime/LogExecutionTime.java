7ab0eaa3ce3c87499f3b9e0e8490681c97d9f9c4
Aayush501 <shrivastavashu501@gmail.com>
2025-02-18 11:41:49 +0530

    [Aayush Kumar Shrivastav] Add . Annotation Problems -> Solution

---
+++ Day06/Annotations/src/main/java/org/annotation/annotationforloggingmethodexecutiontime/LogExecutionTime.java
@@ -0,0 +1,12 @@
+package org.annotation.annotationforloggingmethodexecutiontime;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// Defining the annotation @LogExecutionTime
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogExecutionTime {
+}
